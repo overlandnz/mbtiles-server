605be3d06363d5a59e7834ec57a428aff7dbe0d9
Daniel <daniel@danielwylie.me>
2021-09-26 19:30:49 +1300

    New project

--- MBTilesServer/Program.cs
+++ MBTilesServer/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace mbtiles_server
+namespace MBTilesServer
 {
     public class Program
     {
